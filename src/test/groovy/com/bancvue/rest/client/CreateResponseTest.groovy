41b28dc18a2eb51ca9d82b694de843b964665a0b
Mike Lueders <mike@roadkill.org>
2014-08-01 14:16:56 -0500

    [PPOC-407] added handling of 200 and 204 to CreateResponse

--- src/test/groovy/com/bancvue/rest/client/CreateResponseTest.groovy
+++ src/test/groovy/com/bancvue/rest/client/CreateResponseTest.groovy
@@ -9,6 +9,7 @@ import spock.lang.Specification
 
 import javax.ws.rs.core.GenericType
 import javax.ws.rs.core.Response
+import spock.lang.Unroll
 
 class CreateResponseTest extends Specification {
 
@@ -20,16 +21,22 @@ class CreateResponseTest extends Specification {
 		postResponse = new CreateResponse(clientResponse, new UnexpectedResponseExceptionFactory.Default())
 	}
 
-	def "assertEntityCreatedAndGet with GenericType convert and return entity if status created"() {
+	@Unroll
+	def "assertEntityCreatedAndGet with GenericType convert and return #response if status #statusCode"() {
 		GenericType<String> genericType = new GenericType<String>() {}
-		clientResponse.getStatus() >> HttpStatus.SC_CREATED
-		clientResponse.readEntity(genericType) >> "value"
+		clientResponse.getStatus() >> statusCode
+		if (response) {
+			clientResponse.readEntity(genericType) >> response
+		}
 
-		when:
-		String actualResponse = postResponse.assertEntityCreatedAndGetResponse(genericType)
+		expect:
+		response == postResponse.assertEntityCreatedAndGetResponse(genericType)
 
-		then:
-		"value" == actualResponse
+		where:
+		statusCode               | response
+		HttpStatus.SC_CREATED    | "value"
+		HttpStatus.SC_OK         | "value"
+		HttpStatus.SC_NO_CONTENT | null
 	}
 
 	def "assertEntityCreatedAndGetResponse should throw ConflictException when server returns 409 with no entity" (){
