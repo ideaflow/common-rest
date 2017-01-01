b2c974768b9d4fa0442114abb08c987a30841f46
Mike Lueders <mike@roadkill.org>
2017-01-01 14:58:52 -0600

    move ObjectMapper creation into a factory to improve extensibility

--- src/main/java/com/bancvue/rest/config/ObjectMapperContextResolver.java
+++ src/main/java/com/bancvue/rest/config/ObjectMapperContextResolver.java
@@ -15,45 +15,15 @@
  */
 package com.bancvue.rest.config;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
-import org.joda.time.LocalDateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
-import java.io.IOException;
 
 @Provider
 public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
-	private static ObjectMapper mapper = new ObjectMapper();
 
-	static {
-		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-		mapper.registerModule(new JodaModule());
-		mapper.registerModule(createCustomLocalDateTimeFormatModule());
-	}
-
-	private static SimpleModule createCustomLocalDateTimeFormatModule() {
-		final DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss");
-
-		SimpleModule module = new SimpleModule();
-		module.addSerializer(LocalDateTime.class, new StdSerializer<LocalDateTime>(LocalDateTime.class) {
-			@Override
-			public void serialize(LocalDateTime value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
-				jgen.writeString(value.toString(formatter));
-			}
-		});
-		return module;
-	}
+	private static ObjectMapper mapper = new StandardObjectMapperFactory().createObjectMapper();
 
 	@Override
 	public ObjectMapper getContext(Class<?> type) {
---
+++ src/main/java/com/bancvue/rest/config/StandardObjectMapperFactory.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2017 BancVue, LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bancvue.rest.config;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.io.IOException;
+
+public class StandardObjectMapperFactory {
+
+	public ObjectMapper createObjectMapper() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		mapper.registerModule(new JodaModule());
+		mapper.registerModule(createCustomLocalDateTimeFormatModule("yyyy-MM-dd'T'HH:mm:ss"));
+		return mapper;
+	}
+
+	protected SimpleModule createCustomLocalDateTimeFormatModule(String pattern) {
+		final DateTimeFormatter formatter = DateTimeFormat.forPattern(pattern);
+
+		SimpleModule module = new SimpleModule();
+		module.addSerializer(LocalDateTime.class, new StdSerializer<LocalDateTime>(LocalDateTime.class) {
+			@Override
+			public void serialize(LocalDateTime value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+				jgen.writeString(value.toString(formatter));
+			}
+		});
+		return module;
+	}
+
+}
